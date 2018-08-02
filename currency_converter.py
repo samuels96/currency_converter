e20bf508dc8b966a092f504e20538c28c5b11b1e
samuels96 <samo.seman96>
2018-08-02 12:04:48 +0200

    first

---
+++ currency_converter.py
@@ -0,0 +1,93 @@
+import json, requests
+from builtins import input
+from currency_codes import curr
+import os,sys,time
+
+def remove_last_line():
+    print('\x1b[1A' +'\x1b[2K' + '\x1b[1A')
+def refresh():
+    os.system('cls' if os.name in "nt" else "clear")
+
+bold = '\033[1m'
+e_bold = '\033[0m'
+
+def list_codes():
+    for c in curr:
+        print("Currency: {:45} Code: {:}".format(bold+curr[c]["name"]+e_bold,bold+c+e_bold))
+
+def conversion(key):
+        while True:
+            currency1 = input("\nEnter the code of the currency you want to convert from > ").upper()
+            loop = True
+            for c in curr:
+                if currency1 == c:
+                    loop = False
+            if loop == False:
+                break
+
+        while True:
+            currency2 = input("\nEnter the code of the currency you want to convert to > ").upper()
+            loop = True
+            for c in curr:
+                if currency2 == c:
+                    loop = False
+            if loop == False:
+                while True:
+                    try:
+                        amount = int(input("\nEnter the amount of {} you want to convert > ".format(bold+currency1+e_bold)))
+                        if amount < 0:
+                            continue
+                        else:
+                            break
+                    except:
+                        continue
+                print("\nConverting {} {} to {} ...".format(bold + str(amount),currency1 + e_bold,bold+currency2 + e_bold))
+                break
+
+        url = requests.get("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={}&to_currency={}&apikey={}".format(currency1,currency2,key))
+        j = json.loads(url.text)
+        converted = float(j["Realtime Currency Exchange Rate"]["5. Exchange Rate"])*amount
+        print("\t\t\t      {} {} is {}{:.2f} {}.".format(bold + str(amount),currency1 + e_bold, bold , converted,currency2 + e_bold))
+
+
+def main():
+    while True:
+        try:
+            file = open("api_key.txt","r")
+            key = file.read()
+            file.close()
+            if key == "":
+                os.remove("api_key.txt")
+                continue
+            break
+        except:
+            file = open("api_key.txt","w")
+            key = input("Enter your api-key > ")
+            file.write(key)
+            file.close()
+            break
+
+
+    def menu():
+        choice = input("\nEnter 1 to convert currencies\nEnter 2 to lookup currency codes\n> ")
+        if choice == "1":
+            remove_last_line()
+            remove_last_line()
+            remove_last_line()
+            conversion(key)
+            main()
+        if choice == "2":
+            remove_last_line()
+            refresh()
+            list_codes()
+            input("\nEnter anything to go back > ")
+            refresh()
+            main()
+        else:
+
+            return menu()
+
+    menu()
+
+if __name__ == "__main__":
+    main()
